8b75461b09ae209b63bc0bc44b8673dfa9b0ee14
Ganesh B <ganeshsurfs@gmail.com>
2024-03-16 16:38:11 +0530

    [COMMIT] Adding command functions for most of the linux commands

--- demos/mvp.prototype.function.creation.js
+++ demos/mvp.prototype.function.creation.js
@@ -4,10 +4,11 @@ const { execFile, exec, execFileSync, fork, spawn, spawnSync } = require('child_
 
 let pt = process.cwd();
 let fileName = "ls", args = ["-a"], options = {};
-let ls = new Function('fileName', 'args', 'options', 'spawnSync', `return spawnSync(fileName, args, options || { cwd: "' + ${pt} + '" });`)
-let sl = ((fileName, args, options) => {
-  ls(fileName, args, options, spawnSync)
-})
-sl(fileName, args, options)
+let lsf = new Function('fileName', 'args', 'options', 'spawnSync', `return spawnSync(fileName, args, options || { cwd: "' + ${pt} + '" });`)
+let ls = ((fileName, args, options) => lsf(fileName, args, options, spawnSync))
+
+// USAGE: ls command
+// ls(fileName, args, options)
+
 console.log(ls(fileName, args, options, spawnSync).stdout.toString());
 
