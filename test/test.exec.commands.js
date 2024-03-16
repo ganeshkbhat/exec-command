8291ab3d541c5390cc2e98d6da9d903c4e5ac421
Ganesh B <ganeshsurfs@gmail.com>
2024-03-16 16:42:24 +0530

    [COMMIT] Adding command functions for most of the linux commands

--- test/test.exec.commands.js
+++ test/test.exec.commands.js
@@ -19,13 +19,280 @@
 const { expect } = require('chai');
 
 
-describe('[ tests] Tests to ', () => {
+describe('[commands usage tests] Tests to ', () => {
   let tst, written;
-  before(() => {
-    
-  });
+
+  let commands = [
+    "access", "accton", "aclocal", "acpi", "acpi_available",
+    "acpid", "addr2line", "agetty", "alias", "amixer", "aplay",
+    "aplaymidi", "apropos", "apt", "apt-get", "aptitude", "ar",
+    "arch", "arp", "aspell", "atd", "atrm", "atq", "autoconf",
+    "autoheader", "automake", "autoreconf", "autoupdate", "awk",
+    "banner", "basename", "batch", "bc", "bg", "biff", "bind",
+    "bison", "break", "builtin", "bzcmp", "bzdiff", "bzgrep",
+    "bzip2", "bzless", "cal", "case", "cat", "cc", "ccrypt",
+    "cd", "cfdisk", "chage", "chattr", "chfn", "chgrp", "chkconfig",
+    "chmod", "chown", "chpasswd", "chroot", "chrt", "chsh", "chvt",
+    "cksum", "clear", "cmp", "col", "colcrt", "colrm", "column",
+    "comm", "compress", "continue", "cp", "cpio", "cpp", "cron",
+    "crontab", "csplit", "ctags", "cupsd", "curl", "cut", "cvs",
+    "date", "dc", "dd", "declare", "depmod", "df", "diff", "diff3",
+    "dir", "dirname", "dirs", "disable", "dmesg", "dmesg", "dmidecode",
+    "domainname", "dos2unix", "dosfsck", "dstat", "du", "dump",
+    "dumpe2fs", "dumpkeys", "echo", "ed", "egrep", "eject", "emacs",
+    "enable", "env", "eval", "ex", "exec", "exit", "expand", "expect",
+    "export", "expr", "factor", "fc", "fc-cache", "fc-list", "fdisk",
+    "fg", "fgrep", "file", "find", "finger", "fmt", "fold", "for",
+    "free", "Fun", "function", "g++", "gawk", "gcc", "gdb", "getent",
+    "gpasswd", "grep", "groupadd", "groupdel", "groupmod", "groups",
+    "grpconv", "gs", "tar", "xz", "gz", "gunzip", "gzexe", "gzip",
+    "halt", "hash", "hdparm", "help", "hexdump", "history",
+    "host", "hostid", "hostname", "hostnamectl", "htop",
+    "hwclock", "iconv", "id", "if", "iftop", "ifup",
+    "import", "info", "insmod", "install", "iostat", "iotop", "ip",
+    "ipcrm", "ipcs", "iptables", "iptables-save", "iwconfig",
+    "join", "journalctl", "kill", "last", "less", "let", "ln", "locate",
+    "look", "lsblk", "lshw", "lsmod", "lsof", "lsusb", "mailq", "man",
+    "md5sum", "mkdir", "modinfo", "more", "mount", "mpstat", "mv", "nc",
+    "netstat", "nmcli", "nslookup", "od", "passwd", "paste", "pidof",
+    "ping", "pinky", "pmap", "poweroff", "printf", "ps", "pwd", "ranlib",
+    "rcp", "read", "readelf", "readlink", "reboot", "rename",
+    "reset", "restore", "return", "rev", "rm", "rmdir", "rmmod",
+    "route", "rsync", "sar", "scp", "screen", "script", "scriptreplay",
+    "sdiff", "sed", "select", "seq", "setsid", "shift", "showkey",
+    "shred", "shutdown", "sleep", "source", "sort", "split", "ssh",
+    "strace", "stty", "su", "sudo", "sum", "sync", "systemctl", "tac",
+    "tail", "tee", "time", "top", "touch", "tr", "tracepath",
+    "traceroute", "tree", "tty", "type", "uname", "unexpand", "uniq",
+    "unix2dos", "until", "uptime", "useradd", "usermod", "username",
+    "users", "userdel", "vi", "vmstat", "vnstat", "w", "wall", "watch",
+    "wc", "wget", "whatis", "which", "while", "who", "whoami", "write",
+    "xargs", "xdg-open", "yes", "zdiff", "zdump", "zgrep", "zip", "ls",
+    "find", "kill", "ifconfig", "curl", "diff",
+    "head", "tail", "set", "apropos", "cat", "whereis", "hd", "at",
+    "apt-get", "yumrm", "yum", "cc", "make", "nohup",
+    "jobs", "wait", "atop", "pgrep", "/etc/inittab", "init"
+  ];
+
+  before(() => { });
 
   after(() => { });
+
+  it('should ', () => {
+    let actual = true;
+    let expected = true;
+    expect(actual).to.equal(expected);
+  });
+
+  it('should ', () => {
+    let actual = true;
+    let expected = true;
+    expect(actual).to.equal(expected);
+  });
+
+  it('should ', () => {
+    let actual = true;
+    let expected = true;
+    expect(actual).to.equal(expected);
+  });
+
+  it('should ', () => {
+    let actual = true;
+    let expected = true;
+    expect(actual).to.equal(expected);
+  });
+
+  it('should ', () => {
+    let actual = true;
+    let expected = true;
+    expect(actual).to.equal(expected);
+  });
+
+  it('should ', () => {
+    let actual = true;
+    let expected = true;
+    expect(actual).to.equal(expected);
+  });
+
+  it('should ', () => {
+    let actual = true;
+    let expected = true;
+    expect(actual).to.equal(expected);
+  });
+
+  it('should ', () => {
+    let actual = true;
+    let expected = true;
+    expect(actual).to.equal(expected);
+  });
+
+  it('should ', () => {
+    let actual = true;
+    let expected = true;
+    expect(actual).to.equal(expected);
+  });
+
+  it('should ', () => {
+    let actual = true;
+    let expected = true;
+    expect(actual).to.equal(expected);
+  });
+
+  it('should ', () => {
+    let actual = true;
+    let expected = true;
+    expect(actual).to.equal(expected);
+  });
+
+  it('should ', () => {
+    let actual = true;
+    let expected = true;
+    expect(actual).to.equal(expected);
+  });
+
+  it('should ', () => {
+    let actual = true;
+    let expected = true;
+    expect(actual).to.equal(expected);
+  });
+
+  it('should ', () => {
+    let actual = true;
+    let expected = true;
+    expect(actual).to.equal(expected);
+  });
+
+  it('should ', () => {
+    let actual = true;
+    let expected = true;
+    expect(actual).to.equal(expected);
+  });
+
+  it('should ', () => {
+    let actual = true;
+    let expected = true;
+    expect(actual).to.equal(expected);
+  });
+
+  it('should ', () => {
+    let actual = true;
+    let expected = true;
+    expect(actual).to.equal(expected);
+  });
+
+  it('should ', () => {
+    let actual = true;
+    let expected = true;
+    expect(actual).to.equal(expected);
+  });
+
+  it('should ', () => {
+    let actual = true;
+    let expected = true;
+    expect(actual).to.equal(expected);
+  });
+
+  it('should ', () => {
+    let actual = true;
+    let expected = true;
+    expect(actual).to.equal(expected);
+  });
+
+  it('should ', () => {
+    let actual = true;
+    let expected = true;
+    expect(actual).to.equal(expected);
+  });
+
+  it('should ', () => {
+    let actual = true;
+    let expected = true;
+    expect(actual).to.equal(expected);
+  });
+
+  it('should ', () => {
+    let actual = true;
+    let expected = true;
+    expect(actual).to.equal(expected);
+  });
+
+  it('should ', () => {
+    let actual = true;
+    let expected = true;
+    expect(actual).to.equal(expected);
+  });
+
+  it('should ', () => {
+    let actual = true;
+    let expected = true;
+    expect(actual).to.equal(expected);
+  });
+
+  it('should ', () => {
+    let actual = true;
+    let expected = true;
+    expect(actual).to.equal(expected);
+  });
+
+  it('should ', () => {
+    let actual = true;
+    let expected = true;
+    expect(actual).to.equal(expected);
+  });
+
+  it('should ', () => {
+    let actual = true;
+    let expected = true;
+    expect(actual).to.equal(expected);
+  });
+
+  it('should ', () => {
+    let actual = true;
+    let expected = true;
+    expect(actual).to.equal(expected);
+  });
+
+  it('should ', () => {
+    let actual = true;
+    let expected = true;
+    expect(actual).to.equal(expected);
+  });
+
+  it('should ', () => {
+    let actual = true;
+    let expected = true;
+    expect(actual).to.equal(expected);
+  });
+
+  it('should ', () => {
+    let actual = true;
+    let expected = true;
+    expect(actual).to.equal(expected);
+  });
+
+  it('should ', () => {
+    let actual = true;
+    let expected = true;
+    expect(actual).to.equal(expected);
+  });
+
+  it('should ', () => {
+    let actual = true;
+    let expected = true;
+    expect(actual).to.equal(expected);
+  });
+
+  it('should ', () => {
+    let actual = true;
+    let expected = true;
+    expect(actual).to.equal(expected);
+  });
+
+  it('should ', () => {
+    let actual = true;
+    let expected = true;
+    expect(actual).to.equal(expected);
+  });
 
   it('should ', () => {
     let actual = true;
